f82059912301fb59f5427ed89b6b0db102571085
Sumit Kumar <sumit.k@source.one>
2026-08-15 21:40:51 +0530

    update frontend build

--- frontend/dist/assets/flag-D8e7xRgL.js
+++
@@ -1,6 +0,0 @@
-import{i as e}from"./index-CXXw5Qh0.js";/**
- * @license lucide-react v0.414.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const o=e("Flag",[["path",{d:"M4 15s1-1 4-1 5 2 8 2 4-1 4-1V3s-1 1-4 1-5-2-8-2-4 1-4 1z",key:"i9b6wo"}],["line",{x1:"4",x2:"4",y1:"22",y2:"15",key:"1cm3nv"}]]);export{o as F};
