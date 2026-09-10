288e91e530ca03543f5084ef7e95727a0925fc0d
Sumit Kumar <sumit.k@source.one>
2026-09-10 10:29:13 +0530

    update frontend build

---
+++ frontend/dist/assets/minus-B6bdbxEt.js
@@ -0,0 +1,6 @@
+import{i as s}from"./index-COMW83AJ.js";/**
+ * @license lucide-react v0.414.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const e=s("Minus",[["path",{d:"M5 12h14",key:"1ays0h"}]]);export{e as M};
